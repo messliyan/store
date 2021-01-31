9b1671f8cfb138cf34feebfc79ac88a7c2b33623
lijiabin123 <lijiabin@xinzhili.cn>
2021-01-31 16:58:10 +0800

    dist

--- store-admin-ui/vue.config.js
+++ store-admin-ui/vue.config.js
@@ -20,13 +20,12 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/store" : "/store",
   // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
   outputDir: 'dist',
-  baseUrl: `http://47.97.231.31:8888`,
   // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
   assetsDir: 'static',
   // 是否开启eslint保存检测，有效值：ture | false | 'error'
   lintOnSave: process.env.NODE_ENV === 'development',
   // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
-  productionSourceMap: true,
+  productionSourceMap: false,
   // webpack-dev-server 相关配置
   devServer: {
     host: '0.0.0.0',
@@ -39,6 +38,13 @@ module.exports = {
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      },
+      'https://messliyan.github.io/prod-api':{
+        target: `http://47.97.231.31:8888`,
+        changeOrigin:true,
+        pathRewrite: {
+          ['^' + 'https://messliyan.github.io/prod-api']: ''
         }
       }
     },
